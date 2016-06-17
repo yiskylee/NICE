7424137e9eeb8eecd75052eb2edb90cd20c2fd76
collin_purcell <cpurc002@ucr.edu>
2016-06-17 17:40:03 -0400

    Added gpu_util.h to hold Error checking functions and worked on gpu_svd_solver.cu

---
+++ cpp/include/gpu_util.h
@@ -0,0 +1,10 @@
+void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) { exit(code); }
+   }
+}
+void gpuErrchk(cudaError_t ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
