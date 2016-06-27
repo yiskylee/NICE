7830436302aa9924d9241e1f2bbe9db974dabeba
Jason Booth <booth.j@husky.neu.edu>
2016-06-27 15:28:25 -0400

    Fixes to transpose test file

---
+++ cpp/CMakeFiles/Nice.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Nice.
+# This may be replaced when dependencies are built.
---
+++ cpp/CMakeFiles/googletest.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/googletest"
+  "CMakeFiles/googletest-complete"
+  "gtest/src/googletest-stamp/googletest-install"
+  "gtest/src/googletest-stamp/googletest-mkdir"
+  "gtest/src/googletest-stamp/googletest-download"
+  "gtest/src/googletest-stamp/googletest-update"
+  "gtest/src/googletest-stamp/googletest-patch"
+  "gtest/src/googletest-stamp/googletest-configure"
+  "gtest/src/googletest-stamp/googletest-build"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/googletest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
