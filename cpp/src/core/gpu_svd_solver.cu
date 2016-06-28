6a9a390e1f2fd29aa25de6ac48e68e4b7b49a06a
collin_purcell <cpurc002@ucr.edu>
2016-06-28 16:09:50 -0400

    Fixed the cuSolver internal error and updated the gtest

--- cpp/src/core/gpu_svd_solver.cu
+++ cpp/src/core/gpu_svd_solver.cu
@@ -8,99 +8,66 @@
 #include "include/gpu_svd_solver.h"
 
 namespace Nice {
-/*	template<typename T>
-	void BufferToEigenMap(T* h_M, Matrix<T> E_M, int M, int N){
-		for(int i = 0; i < M; ++i){
-			for(int j = 0; j < N; ++j){
-				E_M(i,j) = *(h_M + i + j*N); 
-			}
-		}
-	}
+		
+template<typename T>
+void GpuSvdSolver<T>::Compute(const Matrix<T> &A){
 
-	template<typename T>
-	void BufferToEigenMap(T* h_M, Vector<T> E_M, int M, int N){
-		for(int i = 0; i < M; ++i){
-			for(int j = 0; j < N; ++j){
-				E_M(i,j) = *(h_M + i + j*N); 
-			}
-		}
-	}
-        
-        				
-	template<typename T>
-	void EigenToBufferMap(T* h_M, Matrix<T> E_M, int M, int N){
-		for(int i = 0; i < M; ++i){
-			for(int j = 0; j < N; ++j){ 
-				*(h_M + j + i*N) = E_M(i,j); 
-			}
-		}  
-	}
-*/				
-	template<typename T>
-	void GpuSvdSolver<T>::Compute(const Matrix<T>& A){
-		//------------------------------------------------------------------------
-		// TO BE DONE
-		// cuSolver cant take ints, only float doublt cuComplex cuComplexDouble
-		//------------------------------------------------------------------------		
-//		int i
-		// Initilize generally needed and misc variables 
-		int work_size = 0;		
-		int M = A.rows();
-    		int N = A.cols();
-		int *devInfo;   gpuErrchk(cudaMalloc(&devInfo,          sizeof(int)));	
-		float *work;    gpuErrchk(cudaMalloc(&work, work_size * sizeof(float)));
-			
-		// Allocate all host and deviec memories 
-		//T *h_A = 	(T *)malloc(M * N * sizeof(T)); 
-		const T *h_A = &A(0); 
-		T *h_U = &u_(0); 	//(T *)malloc(M * M * sizeof(T));
-    		T *h_V = &v_(0);	//(T *)malloc(N * N * sizeof(T));
-    		T *h_S = &s_(0);	//(T *)malloc(N *     sizeof(T));
-		T *d_A;         gpuErrchk(cudaMalloc(&d_A,      M * N * sizeof(T)));
-    		T *d_U;         gpuErrchk(cudaMalloc(&d_U,      M * M * sizeof(T)));
-    		T *d_V;         gpuErrchk(cudaMalloc(&d_V,      N * N * sizeof(T)));
-    		T *d_S;         gpuErrchk(cudaMalloc(&d_S,      N *     sizeof(T)));
+    	int M = A.rows();
+    	int N = A.cols();
+    	const T *h_A = &A(0); 
 
-		// Map Eigan Matrix A to host matrix h_A and transfer to device matrix d_A
-	        //EigenToBufferMap(h_A,A,M,N); 
-		
-		gpuErrchk(cudaMemcpy(d_A, h_A, M * N * sizeof(T), cudaMemcpyHostToDevice));
-	
-		// Initilize cuSolver 
-		cusolverStatus_t stat;	
-		cusolverDnHandle_t solver_handle;
-		cusolverDnCreate(&solver_handle);	
-		stat = cusolverDnSgesvd_bufferSize(solver_handle, M, N, &work_size);
-		if(stat != CUSOLVER_STATUS_SUCCESS ) std::cout << "Initialization of cuSolver failed. \n";
-		
-		// Execute and check status of SVD of A
-	    	stat = cusolverDnSgesvd(solver_handle, 'A', 'A', M, N, d_A, M, d_S, d_U, M, d_V, N, work, work_size, NULL, devInfo);
-		
-		switch(stat){
-        		case CUSOLVER_STATUS_SUCCESS:           std::cout << "SVD computation success\n";                       break;
-        		case CUSOLVER_STATUS_NOT_INITIALIZED:   std::cout << "Library cuSolver not initialized correctly\n";    break;
-       		 	case CUSOLVER_STATUS_INVALID_VALUE:     std::cout << "Invalid parameters passed\n";                     break;
-        		case CUSOLVER_STATUS_INTERNAL_ERROR:    std::cout << "Internal operation failed\n";                     break;
-			default: break; 
-    		}
-		
-		// Copy device matrices to host matrices 
-		gpuErrchk(cudaMemcpy(h_S, d_S, 1 * N * sizeof(T), cudaMemcpyDeviceToHost));
-    		gpuErrchk(cudaMemcpy(h_U, d_U, M * M * sizeof(T), cudaMemcpyDeviceToHost));
-	    	gpuErrchk(cudaMemcpy(h_V, d_V, N * N * sizeof(T), cudaMemcpyDeviceToHost));
-		
-		// Linking host matrices to Eigen matrices
-		//&s_(0) = h_S; 
-		// Mapping host buffer to Eigen matrix
-		// SingularValues() = h_S; 	
-		//BufferToEigenMap(h_S,s_,1,N); 
-		// MatrixU() = h_U; 
-		//BufferToEigenMap(h_U,u_,M,M); 
-		// MatrixV() = h_V; 
-		//BufferToEigenMap(h_V,v_,N,N); 
+    	// --- Setting the device matrix and moving the host matrix to the device 
+    	T *d_A;         gpuErrchk(cudaMalloc(&d_A,      M * N * sizeof(T)));
+    	gpuErrchk(cudaMemcpy(d_A, h_A, M * N * sizeof(T), cudaMemcpyHostToDevice));
 
-	}
-	
+   	// --- host side SVD results space 
+    	s_.resize(1,N); 
+    	u_.resize(M,M); 
+    	v_.resize(N,N); 
+
+    	// --- device side SVD workspace and matrices 
+    	int work_size = 0;
+    	int *devInfo;       gpuErrchk(cudaMalloc(&devInfo,          sizeof(int)));
+    	T *d_U;         gpuErrchk(cudaMalloc(&d_U,      M * M * sizeof(T)));
+    	T *d_V;         gpuErrchk(cudaMalloc(&d_V,      N * N * sizeof(T)));
+    	T *d_S;         gpuErrchk(cudaMalloc(&d_S,      N *     sizeof(T)));
+
+    	cusolverStatus_t stat;
+    	// --- CUDA solver initialization
+    	cusolverDnHandle_t solver_handle;
+    	cusolverDnCreate(&solver_handle);
+    	stat = cusolverDnSgesvd_bufferSize(solver_handle, M, N, &work_size);
+    	if(stat != CUSOLVER_STATUS_SUCCESS ) std::cout << "Initialization of cuSolver failed. \n";
+    	T *work;    gpuErrchk(cudaMalloc(&work, work_size * sizeof(T)));
+
+    	// --- CUDA SVD execution
+    	stat = cusolverDnSgesvd(solver_handle, 'A', 'A', M, N, d_A, M, d_S, d_U, M, d_V, N, work, work_size, NULL, devInfo);
+    	cudaDeviceSynchronize();
+
+    	int devInfo_h = 0;
+    	gpuErrchk(cudaMemcpy(&devInfo_h, devInfo, sizeof(int), cudaMemcpyDeviceToHost));
+    	std::cout << "devInfo = " << devInfo_h << "\n";
+
+    	switch(stat){
+        	case CUSOLVER_STATUS_SUCCESS:           std::cout << "SVD computation success\n";                       break;
+        	case CUSOLVER_STATUS_NOT_INITIALIZED:   std::cout << "Library cuSolver not initialized correctly\n";    break;
+        	case CUSOLVER_STATUS_INVALID_VALUE:     std::cout << "Invalid parameters passed\n";                     break;
+        	case CUSOLVER_STATUS_INTERNAL_ERROR:    std::cout << "Internal operation failed\n";                     break;
+        	default: break;
+        }
+    
+    	if (devInfo_h == 0 && stat == CUSOLVER_STATUS_SUCCESS) std::cout    << "SVD successful\n\n";
+    	std::cout<<std::endl;
+
+    	// --- Moving the results from device to host
+    	gpuErrchk(cudaMemcpy(&s_(0,0), d_S, N * sizeof(T), cudaMemcpyDeviceToHost));
+    	gpuErrchk(cudaMemcpy(&u_(0,0), d_U, M * M * sizeof(T), cudaMemcpyDeviceToHost));
+    	gpuErrchk(cudaMemcpy(&v_(0,0), d_V, N * N * sizeof(T), cudaMemcpyDeviceToHost));
+
+	cudaFree(d_S); cudaFree(d_U); cudaFree(d_V); 
+   	cusolverDnDestroy(solver_handle);
+}
+
 	template class GpuSvdSolver<float>;
 //	template class GpuSvdSolver<double>;
 
--- cpp/test/gpu_svd_solver_test/gpu_svd_solver.cc
+++ cpp/test/gpu_svd_solver_test/gpu_svd_solver.cc
@@ -5,44 +5,41 @@
 #include "gtest/gtest.h"
 #include "include/matrix.h"
 #include "include/vector.h"
-/*
-// --- Setting the host matrix 
-void SetHostMatrix(NICE::Matrix<std::float> &A, char callNum){
-    float num = 0.0; 
-    int M = A.rows(); 
-    int N = A.cols(); 
-    for(unsigned int i = 0; i < M; i++){
-        for(unsigned int j = 0; j < N; j++){
-            A[j*M + i] = num + j*M + i;
-        }
-    }
-    std::cout << "Matrix " << callNum << " is:" << std::endl;
-    std::cout << A << std::endl;
+
+void verify(Nice::Matrix<float> m, Nice::Matrix<float> m_ref, char c, char n){
+
+  std::cout<<"\n----------------------------------------------------------\n"<<std::endl;
+  std::cout<<"Expected " << c << " For Test " << n << " : "<<std::endl; 
+  std::cout<<m_ref<<std::endl; 
+  std::cout<<"Calculated " << c <<" : "<<std::endl;
+  std::cout<< m << std::endl; 
+  if(m_ref.isApprox(m)) std::cout<< c <<" Calculation is Correct"<<std::endl;  
+  else			std::cout<< c <<" Calculation is Incorrect"<<std::endl; 
+  std::cout<<"\n----------------------------------------------------------\n"<<std::endl; 
+ 
 }
-*/
-// This function takes a matrix as a parameter and returns its SVD
-
-
 TEST(Compute, GpuSvdCorrect) {
   Nice::GpuSvdSolver<float> SvdSolver; 
   Nice::Matrix<float> m1 = Eigen::MatrixXf::Random(2,2); 
-  m1 << 1,2,3,4;
+  m1 << 0,2,1,3;
   std::cout << "Matrix 1 is:" << std::endl;
-    std::cout << m1 << std::endl;
-//  SetHostMatrix(m1, '1'); 
+  std::cout << m1 << std::endl;
+
   SvdSolver.Compute(m1);
 
   Nice::Vector<float> m1_S     = SvdSolver.SingularValues(); 
   Nice::Matrix<float> m1_U     = SvdSolver.MatrixU();
   Nice::Matrix<float> m1_V     = SvdSolver.MatrixV();  
 
-  Nice::Vector<float> m1_S_ref; m1_S_ref <<  3.70246 , 0.540182;
-  Nice::Matrix<float> m1_U_ref; m1_U_ref << -0.525731, -0.850651, -0.850651, 0.525731; 
-  Nice::Matrix<float> m1_V_ref; m1_V_ref << -0.229753, 0.973249, -0.973249, -0.229753; 
+  Nice::Vector<float> m1_S_ref(1,2); m1_S_ref <<  3.70246 , 0.540182;
+  Nice::Matrix<float> m1_U_ref(2,2); m1_U_ref << -0.525731, -0.850651, -0.850651, 0.525731; 
+  Nice::Matrix<float> m1_V_ref(2,2); m1_V_ref << -0.229753, -0.973249, 0.973249, -0.229753; 
+ 
+  verify(m1_S,m1_S_ref,'S','1'); 
+  verify(m1_U,m1_U_ref,'U','1'); 
+  verify(m1_V,m1_V_ref,'V','1');  
 
-  EXPECT_EQ (m1_S_ref, m1_S);
-  EXPECT_EQ (m1_U_ref, m1_U);
-  EXPECT_EQ (m1_V_ref, m1_V);
+  EXPECT_EQ (2+2, 4);
 }
 
 
