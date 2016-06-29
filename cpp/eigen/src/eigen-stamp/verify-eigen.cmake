6f5ed5ab16d98749579e64dd8c23b672c05a2833
yiskylee <yiskylee@gmail.com>
2016-06-29 16:36:58 -0400

    Delete the rest cmake files

--- cpp/eigen/src/eigen-stamp/verify-eigen.cmake
+++
@@ -1,4 +0,0 @@
-set(file "/home/jason.b/Desktop/Github/NICE/cpp/eigen/src/3.2.8.tar.bz2")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
