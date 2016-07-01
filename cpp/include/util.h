a2353a92470db5688f22234eec261c7c428a3315
yiskylee <yiskylee@gmail.com>
2016-07-01 16:21:23 -0400

    refactor develop branch

--- cpp/include/util.h
+++ cpp/include/util.h
@@ -26,13 +26,29 @@
 #include <string>
 #include "include/matrix.h"
 #include "include/vector.h"
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <algorithm>
 
 namespace Nice {
 
 namespace util {
 template<typename T>
 Matrix<T> FromFile(const std::string &input_file_path,
-                   int num_rows, int num_cols);
+                   int num_rows, int num_cols) {
+  std::ifstream input_file(input_file_path, std::ifstream::in);
+  Matrix<T> m(num_rows, num_cols);
+  if (input_file) {
+    for (int i = 0; i < num_rows; i++)
+      for (int j = 0; j < num_cols; j++)
+        input_file >> m(i, j);
+    return m;
+  } else {
+    std::cerr << "Cannot open file " + input_file_path + ", exiting...";
+    exit(1);
+  }
+}
 
 }  // namespace util
 
