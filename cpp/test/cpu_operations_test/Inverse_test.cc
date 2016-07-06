dd4b3d5e08306b5f3bcb95dae10fba6d8beeea80
Leslie Chase <chasel2@rpi.edu>
2016-07-06 13:00:33 -0400

    Adjusted test file to use ASSERT_NEAR instead of EXPECT_EQ

--- cpp/test/cpu_operations_test/Inverse_test.cc
+++ cpp/test/cpu_operations_test/Inverse_test.cc
@@ -47,7 +47,7 @@ class InverseTest : public ::testing::Test {
 };
 
 
-typedef ::testing::Types<int, float, double> MyTypes;
+typedef ::testing::Types</*int,*/float, double> MyTypes;
 TYPED_TEST_CASE(InverseTest, MyTypes);
 
 TYPED_TEST(InverseTest, InverseFunctionality) {
@@ -62,7 +62,7 @@ TYPED_TEST(InverseTest, InverseFunctionality) {
   this->GetInverse();
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      EXPECT_EQ(this->output(i, j), this->correct(i, j));
+      ASSERT_NEAR(this->output(i, j), this->correct(i, j), 0.0001);
     }
   }
 }
