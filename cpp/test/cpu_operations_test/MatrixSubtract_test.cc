19fe364a3b74e8d1603ec3454af9557df537ea3d
ademosthene <ademosthene@me.com>
2016-07-08 14:17:02 -0400

    Code style update

--- cpp/test/cpu_operations_test/MatrixSubtract_test.cc
+++ cpp/test/cpu_operations_test/MatrixSubtract_test.cc
@@ -1,3 +1,25 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 Northeastern University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <stdio.h>
 #include <iostream>
 #include "Eigen/Dense"
@@ -22,10 +44,9 @@ typedef ::testing::Types<int, double, float> MyTypes;
 TYPED_TEST_CASE(MatrixSubtractTest, MyTypes);
 
 TYPED_TEST(MatrixSubtractTest, MatrixSubtractFunctionality) {
-  this->m1.resize(2,2);
-  this->m2.resize(2,2);
- // this->result.resize(2,2);
-  this->testMatrix.resize(2,2);
+  this->m1.resize(2, 2);
+  this->m2.resize(2, 2);
+  this->testMatrix.resize(2, 2);
   this->m1 << 2, 3,
               4, 5;
   this->m2 << 1, 2,
@@ -37,8 +58,8 @@ TYPED_TEST(MatrixSubtractTest, MatrixSubtractFunctionality) {
 }
 
 TYPED_TEST(MatrixSubtractTest, DifferentSizeMatrix) {
-  this->m1.resize(2,2);
-  this->m2.resize(3,2);
+  this->m1.resize(2, 2);
+  this->m2.resize(3, 2);
   this->m1.setZero();
   this->m2.setZero();
   ASSERT_DEATH(this->MatrixSubtract(), ".*");
