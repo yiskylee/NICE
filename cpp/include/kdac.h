78390d1721c64077cef5c1bf3d8e8730b61d135a
yiskylee <yiskylee@gmail.com>
2016-07-25 16:54:45 -0400

    add test dir and file

--- cpp/include/kdac.h
+++ cpp/include/kdac.h
@@ -31,9 +31,138 @@
 
 #ifndef CPP_INCLUDE_KDAC_H
 #define CPP_INCLUDE_KDAC_H
-namespace NICE {
+namespace Nice {
+template<typename T>
 class KDAC {
+ public:
+  /// This is the default constructor for KDAC
+  /// Number of clusters c and reduced dimension q will be both set to 2
+  KDAC() {
+    c_ = 2;
+    q_ = 2;
+  }
+  /// This is the KDAC constructor that takes one parameter c
+  /// \param c
+  /// Number of clusters c, the reducded dimension q will be set to c
+  KDAC(int c) {
+    // reduced dimension q by default equals to k
+    SetCQ(c, c);
+  }
+  /// This is the KDAC constructor that takes both number of clusters c and
+  /// reduced dimension q
+  /// \param c
+  /// Number of clusters c
+  /// \param q
+  /// Reduced dimension q
+  KDAC(int c, int q) {
+    SetCQ(c, q);
+  }
 
+  /// This function creates the first clustering result
+  /// \param input_matrix
+  /// The input matrix of n samples and d features where each row
+  /// represents a sample
+  /// \return
+  /// It only generates the clustering result but does not returns it
+  /// Users can use Predict() to get the clustering result returned
+  void Fit(const Matrix<T> &input_matrix) {
+    // Following the pseudo code in Algorithm 1 in the paper
+    Init(input_matrix);
+    u_matrix_ = GenU();
+  }
+
+  /// This function creates the alternative clustering result
+  /// Users can change c and q using other Fit() functions
+  /// Must be called after \ref Fit(const Matrix<T> &input_matrix)
+  void Fit(void) {
+
+  }
+
+  /// This function creates the alternative clustering result
+  /// with user-specified number of clusters c
+  /// reduced dimension q would be set to equal to c
+  /// Must be called after \ref Fit(const Matrix<T> &input_matrix)
+  /// \param c
+  /// Number of clusters c
+  /// \sa
+  /// \ref Fit(const int c, const int q)
+  void Fit(const int c) {
+    SetCQ(c, c);
+    Fit();
+  }
+
+  /// This function creates the alternative clustering result
+  /// with user-specified number of clusters c and reduced dimension q
+  /// Must be called after \ref Fit(const Matrix<T> &input_matrix)
+  /// \param c
+  /// Number of clusters c
+  /// \param q
+  /// Reduced dimension q
+  /// \sa
+  /// \ref Fit(int c)
+  void Fit(const int c, const int q) {
+    SetCQ(c, q);
+    Fit();
+  }
+
+  /// Running Predict() after Fit() returns
+  /// the current clustering result as a Vector of T
+  /// \return
+  /// A NICE vector of T that specifies the clustering result
+  Vector<T> Predict(void) {
+
+  }
+
+
+ private:
+  int c_;  // cluster number c
+  int q_;  // reduced dimension q
+  int n_;  // number of samples in input data X
+  int d_;  // input data X dimension d
+  bool u_converge;  // If matrix U reaches convergence, false by default
+  bool v_converge;  // If matrix V reaches convergence, false by default
+  Matrix<T> x_matrix_;  // Input matrix X (n by d)
+  Matrix<T> w_matrix_;  // Transformation matrix W (d by q). Initialized to I
+  Matrix<bool> y_matrix_;  // Labeling matrix Y (n by (c0 + c1 + c2 + ..))
+  Matrix<T> d_matrix_;  // Diagonal degree matrix D (n by n)
+  Matrix<T> k_matrix_;  // Kernel matrix K (n by n)
+  Matrix<T> u_matrix_;  // Soft labeling matrix U (n by c)
+  Matrix<T> h_matrix_;  // Centering matrix (n by n)
+  Vector<T> clustering_result;  // Current clustering result
+
+
+  // Initialization
+  void Init(const Matrix<T> &input_matrix) {
+    x_matrix_ = input_matrix;
+    n_ = input_matrix.rows();
+    d_ = input_matrix.cols();
+    w_matrix_ = Matrix<T>::Identity(d_, q_);
+    y_matrix_ = Matrix<bool>::Constant(n_, c_, 0);
+    d_matrix_ = Matrix<T>::Constant(n_, n_, 0);
+    k_matrix_ = Matrix<T>::Constant(n_, n_, 0);
+    u_matrix_ = Matrix<T>::Constant(n_, c_, 0);
+    h_matrix_ = Matrix<T>::Identity(n_, n_)
+        - Matrix<T>::Constant(n_, n_, 1) / float(n_);
+    clustering_result = Vector<T>::Zero(n_);
+    u_converge = false;
+    v_converge = false;
+  }
+
+  // Check if q is not bigger than c
+  void SetCQ(int c, int q) {
+    if (q <= c) {
+      c_ = c;
+      q_ = q;
+    } else {
+      std::cerr <<
+          "Reduced dimension q cannot exceed cluster number c" << std::endl;
+      exit(1);
+    }
+  }
+
+  Matrix<T> GenU(void) {
+    return Matrix<T>::Zero(n_, c_);
+  }
 
 };
 }  // namespace NICE
---
+++ cpp/test/kdac_test.cc
@@ -0,0 +1,62 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2016 Northeastern University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+#include <stdio.h>
+#include <iostream>
+#include "Eigen/Dense"
+#include "gtest/gtest.h"
+#include "include/cpu_operations.h"
+#include "include/kdac.h"
+#include "include/util.h"
+#include "include/matrix.h"
+#include "include/vector.h"
+
+template<typename T>
+class KDACTest : public ::testing::Test {
+ protected:
+  Nice::Matrix<T> data_matrix_;
+  std::shared_ptr<Nice::KDAC<T>> kdac_;
+  int c_;
+  int n_;
+  int d_;
+
+  virtual void Setup() {
+    data_matrix_ = Nice::util::FromFile<T>(
+        "../test/data_for_test/kdac/data_matrix_40_2");
+    c_ = 2;
+    kdac_ = std::make_shared<Nice::KDAC<T>>(c_);
+  }
+
+};
+
+typedef ::testing::Types<float, int, long, double> AllTypes;
+typedef ::testing::Types<int, long> IntTypes;
+typedef ::testing::Types<float, double> FloatTypes;
+
+
+TYPED_TEST_CASE(KDACTest, FloatTypes);
+
+TYPED_TEST(KDACTest, Init) {
+  this->kdac_->Fit(this->data_matrix_);
+}
+
